1b230002cc57f31f8298bbe4177ef194de1bcf7e
Thomas Gruber <tgr@lookat.at>
2024-04-18 22:49:54 +0200

    update

--- setup_env.ps1
+++ setup_env.ps1
@@ -1,5 +1,31 @@
-if ( -Not (Test-Path "venv") ){ 
-    # Create virtual environment
-    python -m venv venv
-    python.exe -m pip install --upgrade pip
+# Define the path for the new virtual environment
+$envPath = ".\venv"
+
+# Define the path to the requirements.txt file
+$requirementsPath = ".\requirements.txt"
+
+# Check if the virtual environment already exists
+if (Test-Path -Path $envPath) {
+    Write-Output "Virtual environment already exists."
+} else {
+    # Create the virtual environment
+    python -m venv $envPath
+    Write-Output "Virtual environment created at $envPath"
 }
+
+# Activate the virtual environment
+# Note: The path to the activation script varies between platforms and Python versions.
+# For Windows, it's usually '.\venv\Scripts\Activate.ps1'. Adjust if necessary.
+$activateScript = Join-Path -Path $envPath -ChildPath "Scripts\Activate.ps1"
+. $activateScript
+
+# Install packages from requirements.txt
+if (Test-Path -Path $requirementsPath) {
+    pip install -r $requirementsPath
+    Write-Output "Packages installed from $requirementsPath"
+} else {
+    Write-Output "requirements.txt not found."
+}
+
+# Deactivate the virtual environment
+deactivate
